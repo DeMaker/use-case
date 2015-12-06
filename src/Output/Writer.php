9c6622301983bfdf957f7050b4f5747ab4893aae
fen <fen@desmart.com>
2015-12-06 20:39:21 +0100

    Make the file writer source-aware (PSR-4)

--- src/Output/Writer.php
+++ src/Output/Writer.php
@@ -3,17 +3,29 @@
 namespace Fojuth\Stamp\Output;
 
 use Fojuth\Stamp\Declaration;
+use Fojuth\Stamp\Config\Loader;
 
 /**
  * Creates the target file.
  */
 class Writer
 {
+
+    /**
+     * @var Loader
+     */
+    protected $config;
+
+    public function __construct(Loader $config)
+    {
+        $this->config = $config;
+    }
+
     public function makeClass(Declaration $declaration, $content)
     {
         $fqnArray = explode('\\', $declaration->getFqn());
         $className = array_pop($fqnArray);
-        $dir = join('/', $fqnArray);
+        $dir = $this->getDir($fqnArray);
         $path = $this->getFilePath($dir, $className);
 
         if (true === file_exists($path)) {
@@ -30,5 +42,26 @@ class Writer
     protected function getFilePath($dir, $className)
     {
         return $dir . '/' . $className . '.php';
+    }
+
+    protected function getDir(array $nsArray)
+    {
+        $ns = join('\\', $nsArray);
+
+        $sources = $this->config->getSources();
+
+        if (true === is_array($sources)) {
+            foreach ($sources as $sourceNamespace => $dir) {
+                if (strstr($ns, $sourceNamespace)) {
+                    $nsToReplace = explode('\\', $sourceNamespace);
+
+                    array_splice($nsArray, 0, count($nsToReplace), [$dir]);
+
+                    break;
+                }
+            }
+        }
+
+        return join('/', $nsArray);
     }
 }
--- tests/Output/WriterTest.php
+++ tests/Output/WriterTest.php
@@ -2,6 +2,7 @@
 
 namespace tests\Output;
 
+use Fojuth\Stamp\Config\Loader;
 use Fojuth\Stamp\Declaration;
 use Fojuth\Stamp\Output\Writer;
 
@@ -18,6 +19,10 @@ class WriterTest extends \PHPUnit_Framework_TestCase
         unlink('Somedir/Foo.php');
         rmdir('Somedir');
 
+        @ unlink('wat/SomeDir/Baz.php');
+        @ rmdir('wat/SomeDir');
+        @ rmdir('wat');
+
         @ unlink('Foo/Bar/Baz.php');
         @ rmdir('Foo/Bar');
         @ rmdir('Foo');
@@ -31,7 +36,9 @@ class WriterTest extends \PHPUnit_Framework_TestCase
         $declaration = new Declaration('baz', 'Foo\Bar\Baz');
         $content = 'foo bar baz';
 
-        $writer = new Writer;
+        $config = $this->prophesize(Loader::class);
+
+        $writer = new Writer($config->reveal());
         $writer->makeClass($declaration, $content);
 
         $this->assertTrue(file_exists($path = 'Foo/Bar/Baz.php'));
@@ -46,10 +53,32 @@ class WriterTest extends \PHPUnit_Framework_TestCase
         $declaration = new Declaration('baz', 'Somedir\Foo');
         $content = 'foo bar baz';
 
-        $writer = new Writer;
+        $config = $this->prophesize(Loader::class);
+
+        $writer = new Writer($config->reveal());
         $writer->makeClass($declaration, $content);
 
         $this->assertTrue(file_exists($path = 'Somedir/Foo.php'));
         $this->assertEquals('123', file_get_contents($path));
+    }
+
+    /**
+     * @test
+     */
+    public function it_sets_proper_path_from_config()
+    {
+        $declaration = new Declaration('baz', 'Foo\Bar\SomeDir\Baz');
+
+        $config = $this->prophesize(Loader::class);
+        $config->getSources()->willReturn([
+            'Something' => 'nope',
+            'Foo\Bar' => 'wat',
+            'Not\Important' => 'hello',
+        ]);
+
+        $writer = new Writer($config->reveal());
+        $writer->makeClass($declaration, '');
+
+        $this->assertTrue(file_exists('wat/SomeDir/Baz.php'));
     }
 }
