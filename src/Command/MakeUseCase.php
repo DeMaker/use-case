2da83a95f38876d5648a3b4a6b0dff3f00ceb8c6
Michal Golon <gollum@desmart.com>
2015-12-11 17:33:40 +0100

    Updates command class.

--- src/Command/MakeUseCase.php
+++ src/Command/MakeUseCase.php
@@ -1,28 +1,38 @@
-<?php namespace DeSmart\DeMaker\UseCase\Command;
+<?php
+
+namespace DeSmart\DeMaker\UseCase\Command;
 
 use DeSmart\DeMaker\Core\Dispatcher\Dispatcher;
-use DeSmart\DeMaker\UseCase\Schema\DTOBuildStrategy;
+use DeSmart\DeMaker\UseCase\Schema\CommandBuildStrategy;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class MakeUseCase extends Command
 {
+    /**
+     * @return void
+     */
     protected function configure()
     {
-        $this->setName('make')
+        $this->setName('use-case')
              ->addArgument('fqn', InputArgument::REQUIRED, 'FQN of the target class')
              ->addOption('inputProperties', 'i', InputOption::VALUE_OPTIONAL, 'Properties passed to command (comma separated)');
     }
 
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return void
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $results = (new Dispatcher(new DTOBuildStrategy($input)))->run();
+        $dispatcherResponses = (new Dispatcher(new CommandBuildStrategy($input)))->run();
 
-//        foreach ($results as $result) {
-//            $declaration = $result['declaration'];
-//
-//            $output->writeln("Generated {$declaration->getAlias()} ({$declaration->getFqn()}) at {$result['path']}");
-//        }
+        foreach ($dispatcherResponses as $response) {
+            $output->writeln("Generated command {$response->getFqn()} at {$response->getPath()}");
+        }
     }
 }
