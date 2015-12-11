43f5101a7784a5a5f17f4cb48c152598af5624ef
Lucjan Wilczewski <lucjan@desmart.com>
2015-12-11 15:54:37 +0100

    Adds DTOBuildStrategy

---
+++ src/Schema/DTOBuildStrategy.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace DeSmart\DeMaker\Core\Schema;
+
+use DeSmart\DeMaker\Core\Schema\BuildStrategyInterface;
+use Memio\Model\Argument;
+use Memio\Model\Method;
+use Memio\Model\Object;
+use Memio\Model\Property;
+use Symfony\Component\Console\Input\InputInterface;
+
+class DTOBuildStrategy implements BuildStrategyInterface
+{
+    protected $fqn;
+    protected $properties = [];
+
+    /**
+     * @param InputInterface $input
+     */
+    public function __construct(InputInterface $input)
+    {
+        $this->fqn = $input->getArgument('fqn');
+
+        if ($input->hasOption('inputProperties')) {
+            $this->properties = explode(',', $input->getOption('inputProperties'));
+        }
+    }
+
+    /**
+     * @return Object[]
+     */
+    public function make()
+    {
+        $command = Object::make($this->fqn);
+
+        $construct = new Method('__construct');
+        $construct->makePublic();
+
+        $command->addMethod($construct);
+
+        $constructBodyElements = [];
+
+        foreach($this->properties as $property) {
+            list($propertyName, $propertyType) = $this->getPropertyDefinition($property);
+
+            $newProperty = new Property($propertyName);
+            $newProperty->makePrivate();
+
+            $command->addProperty($newProperty);
+
+            $argument = new Argument($propertyType, $propertyName);
+            $construct->addArgument($argument);
+
+            $constructBodyElements[] = sprintf('$this->%1$s = $%1$s;', $propertyName);
+
+            $newMethod = new Method(sprintf('get%s', ucfirst($propertyName)));
+            $newMethod->makePublic();
+
+            $command->addMethod($newMethod);
+        }
+
+        $construct->setBody(implode("\n", $constructBodyElements));
+
+        return $command;
+    }
+
+    /**
+     * @param $property
+     * @return array
+     */
+    private function getPropertyDefinition($property)
+    {
+        return explode(':', $property);
+    }
+}
