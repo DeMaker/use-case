986ba03a2812683ced040e975f2b0eafc03d1567
Lucjan Wilczewski <lucjan@desmart.com>
2015-12-11 16:54:18 +0100

    Adds CommandHandlerBuilderStrategy

---
+++ src/Schema/CommandHandlerBuildStrategy.php
@@ -0,0 +1,64 @@
+<?php namespace DeSmart\DeMaker\UseCase\Schema;
+
+use DeSmart\DeMaker\Core\Schema\BuildStrategyInterface;
+use Memio\Model\Argument;
+use Memio\Model\Method;
+use Memio\Model\Object;
+use Memio\Model\Type;
+use Symfony\Component\Console\Input\InputInterface;
+
+class CommandHandlerBuildStrategy implements BuildStrategyInterface
+{
+    protected $commandFqn;
+    protected $handlerFqn;
+    protected $responseFqn;
+    protected $responseProperties;
+
+    /**
+     * @param InputInterface $input
+     */
+    public function __construct(InputInterface $input)
+    {
+        $this->commandFqn = $input->getArgument('fqn');
+        $this->handlerFqn = sprintf('%sHandler', $input->getArgument('fqn'));
+        $this->responseFqn = sprintf('%sResponse', $input->getArgument('fqn'));
+
+        if ($input->hasOption('outputProperties')) {
+            $this->responseProperties = explode(',', $input->getOption('outputProperties'));
+        }
+    }
+
+    /**
+     * @return Object[]
+     */
+    public function make()
+    {
+        $handler = Object::make($this->handlerFqn);
+
+        $handle = new Method('handle');
+        $handle->makePublic();
+
+        $command = new Argument($this->commandFqn, 'command');
+        $handle->addArgument($command);
+
+        $responseArguments = implode(
+            ', ',
+            array_map(
+                function ($argument) {
+                    list($argumentName, $argumentType) = explode(':', $argument);
+
+                    return sprintf('$%s', $argumentName);
+                },
+                $this->responseProperties
+            )
+        );
+
+        $body = str_replace("\t", "    ", sprintf("\t\treturn new %s(%s);", $this->responseFqn, $responseArguments));
+
+        $handle->setBody($body);
+
+        $handler->addMethod($handle);
+
+        return [$handler];
+    }
+}
---
+++ tests/Schema/CommandHandlerBuildStrategyTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace tests\DeSmart\DeMaker\UseCase\Schema;
+
+class CommandHandlerBuildStrategyTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Symfony\Component\Console\Input\InputInterface
+     */
+    private $input;
+
+    private $buildStrategy;
+
+    private $fqn = 'Bar\Foo\WatCommand';
+
+    private $expectedCommandFqn = 'Bar\Foo\WatCommand';
+
+    private $expectedResponseFqn = 'Bar\Foo\WatCommandResponse';
+
+    private $expectedHandlerFqn = 'Bar\Foo\WatCommandHandler';
+
+    public function setUp()
+    {
+        $this->input = $this->prophesize(\Symfony\Component\Console\Input\InputInterface::class);
+        $this->input->getArgument('fqn')->willReturn($this->fqn);
+        $this->input->hasOption('outputProperties')->willReturn(true);
+        $this->input->getOption('outputProperties')->willReturn('firstname:string,lastname:string,dob:\Carbon\Carbon');
+
+        $this->buildStrategy = new \DeSmart\DeMaker\UseCase\Schema\CommandHandlerBuildStrategy($this->input->reveal());
+    }
+
+    /** @test */
+    public function it_is_initializable()
+    {
+        $this->assertInstanceOf(\DeSmart\DeMaker\UseCase\Schema\CommandHandlerBuildStrategy::class, $this->buildStrategy);
+    }
+
+    /** @test */
+    public function it_implements_build_strategy_interface()
+    {
+        $this->assertInstanceOf(\DeSmart\DeMaker\Core\Schema\BuildStrategyInterface::class, $this->buildStrategy);
+    }
+
+    /** @test */
+    public function it_makes_handler_schema()
+    {
+        /** @var \Memio\Model\Object $handler */
+        $handler = $this->buildStrategy->make()[0];
+
+        $this->assertInstanceOf(\Memio\Model\Object::class, $handler);
+    }
+
+    /** @test */
+    public function it_makes_command_with_defined_fully_qualified_name()
+    {
+        /** @var \Memio\Model\Object $handler */
+        $handler = $this->buildStrategy->make()[0];
+
+        $this->assertEquals($this->expectedHandlerFqn, $handler->getFullyQualifiedName());
+    }
+
+    /** @test */
+    public function it_makes_handler_which_handles_command()
+    {
+        /** @var \Memio\Model\Object $handler */
+        $handler = $this->buildStrategy->make()[0];
+
+        $methods = $handler->allMethods();
+
+        /** @var \Memio\Model\Method $handle */
+        $handle = array_shift($methods);
+        $this->assertEquals('handle', $handle->getName());
+        $this->assertEquals('public', $handle->getVisibility());
+
+        $arguments = $handle->allArguments();
+        $this->assertCount(1, $arguments);
+
+        /** @var \Memio\Model\Argument $command */
+        $command = array_shift($arguments);
+        $this->assertEquals($this->expectedCommandFqn, $command->getType());
+        $this->assertEquals('command', $command->getName());
+
+        $handlerBody = "        return new {$this->expectedResponseFqn}(\$firstname, \$lastname, \$dob);";
+
+        $this->assertEquals($handlerBody, $handle->getBody());
+    }
+}
