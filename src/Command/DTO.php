a38ae2df80557421b15598ff63736f70e3feafe1
fen <fen@desmart.com>
2015-12-11 16:35:00 +0100

    Remove obsolete files

--- src/Command/DTO.php
+++ src/Command/DTO.php
@@ -2,19 +2,18 @@
 
 namespace DeSmart\DeMaker\Core\Command;
 
-use DeSmart\DeMaker\Core\Dispatcher;
+use DeSmart\DeMaker\Core\Dispatcher\Dispatcher;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class Make extends Command
+class Popo extends Command
 {
     protected function configure()
     {
-        $this->setName('make')
-            ->addArgument('alias', InputArgument::REQUIRED, 'What to generate')
+        $this->setName('dto')
             ->addArgument('fqn', InputArgument::REQUIRED, 'FQN of the target class')
             ->addOption('properties', 'p', InputOption::VALUE_REQUIRED, 'Properties to generate (comma separated)')
             ->addOption('getters', 'g', InputOption::VALUE_NONE, 'Generate getters')
--- src/Template/Exception/DefinitionNotFoundException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace DeSmart\DeMaker\Core\Template\Exception;
-
-class DefinitionNotFoundException extends \LogicException
-{
-}
